2427dc0c057ec17b702ad4550b57a8220f0535f7
Laurent Prevost <prevole@prevole.ch>
2017-12-18 12:10:13 +0100

    Project creation - video 01

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by lprevost on 18.12.17.
+//  Copyright © 2017 prevole.ch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
