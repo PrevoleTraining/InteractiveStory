7b5712e38882a1aa7e00768d0bd0887cd408cd4a
Laurent Prevost <prevole@prevole.ch>
2017-12-23 15:38:46 +0100

    Video 22 - Added sounds URL

---
+++ InteractiveStory/Audio.swift
@@ -0,0 +1,28 @@
+//
+//  Audio.swift
+//  InteractiveStory
+//
+//  Created by lprevost on 23.12.17.
+//  Copyright © 2017 prevole.ch. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+extension Story {
+    var soundEffectName: String {
+        switch self {
+        case .droid, .home: return "HappyEnding"
+        case .monster: return "Ominous"
+        default: return "PageTurn"
+        }
+    }
+    var soundEffectURL: URL {
+        let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+        return URL(fileURLWithPath: path)
+    }
+}
+
+class SoundEffectsPlayer {
+    var sound: SystemSoundID = 0
+}
